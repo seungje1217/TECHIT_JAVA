229f8cbaba1851fc2f0f6e5fed6957439de442e3
LIM <sjlim1217@naver.com>
2024-04-29 16:25:03 +0900

    4.29

---
+++ src/Loops.java
@@ -0,0 +1,20 @@
+public class Loops {
+    public static void main(String[] args) {
+
+//        for (int i=0; i<10;i++){
+//            System.out.println(i);
+//        }
+        int b=10;
+
+//        while (b>0){
+//            System.out.println(b);
+//            b--;
+//        }
+
+        // do while은 값을 먼저 찍음
+        do{
+            System.out.println(b);
+            b--;
+        }while(b>0);
+    }
+}
